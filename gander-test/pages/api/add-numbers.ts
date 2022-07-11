6817d27e21206f29bcd0d6c7aa83c401908734ac
Marco Saglietto <saglietto_marco@yahoo.com>
2022-07-11 12:38:02 -0700

    fixed type error from vercel deploy

--- gander-test/pages/api/add-numbers.ts
+++ gander-test/pages/api/add-numbers.ts
@@ -18,7 +18,7 @@ export default function handler(
             let result = addNumbersController.addTwoNumbers(req.body)
             res.status(200).json({ result: result })
         } catch (error) {
-            res.status(500).json({ message: error.message as string })
+            res.status(500).json({ message: 'internal server error' })
         }
     } else {
         // Handle any other HTTP method
